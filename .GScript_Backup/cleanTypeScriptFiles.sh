c2c77891000e1f7271bdd17c85fcd740590cb505
Ghasak <mdotgh@gmail.com>
2021-06-04 01:13:32 +0900

    updated my cleanTypeScript for typescript

--- .GScript_Backup/cleanTypeScriptFiles.sh
+++ .GScript_Backup/cleanTypeScriptFiles.sh
@@ -10,23 +10,35 @@ function bannerSimple() {
 }
 
 function cleanTypeScriptFilesInDir() {
-    cat <<EOF
-        This is a script will search for redandent file and clean it which match
-        a pattern given inside top of the file that we want to clean
-            Parameter: It asks you for the word located in the top of the file to
-            identify and not remove other necessary similar files.
+
+cat  << EOF
+---------------------------------------------------------------------
+▀█▀ █▄█ █▀█ █▀▀ █▀ █▀▀ █▀█ █ █▀█ ▀█▀   █▀▀ █░░ █▀▀ ▄▀█ █▄░█ █▀▀ █▀█
+░█░ ░█░ █▀▀ ██▄ ▄█ █▄▄ █▀▄ █ █▀▀ ░█░   █▄▄ █▄▄ ██▄ █▀█ █░▀█ ██▄ █▀▄
+---------------------------------------------------------------------
+                      =  RUN AS SCRIPT =
+This is a script will search for redandent file and clean it which match
+a pattern given inside top of the file that we want to clean
+
+    Parameter: It asks you for the word located in the top of the file to
+               identify and not remove other necessary similar files.
+
+---------------------------------------------------------------------
 EOF
-    bannerSimple " Running the script " "*"
-    read -p "Which file you wish to clean from the directory [Default:Uptime] ... " SEARCHING_WORD
+    bannerSimple " Running the script " "*" | lolcat
+    echo ""
+    echo -n "=> [+] " | lolcat && echo -n "  " | lolcat && read -p "What is the redundant file name to be cleaned [Default:typescript] ... "  FILETYPE
+    FILETYPE=$(echo "${FILETYPE:=typescript}")
+    echo -n "=> [+] " | lolcat && echo -n "ﯨ  " | lolcat   && read -p "First word at second line of file is : [Default:Uptime] ... "  SEARCHING_WORD
     SEARCHING_WORD=$(echo "${SEARCHING_WORD:=Uptime}")
-    bannerSimple " [O] Will remove the following files " "+"
+    bannerSimple " [O]   The following files Will be removed [Type]: ${FILETYPE} " "+" | lolcat
     local NUMBER_OF_FILES=0
-    for file in $(find . -type f -name 'typescript' -ls); do
+    for file in $(find . -type f -name $FILETYPE -ls); do
         if [ -f $file ]; then
             CHECK1=$(head $file | grep $SEARCHING_WORD | awk -F ':' '{print $1}')
             let NUMBER_OF_FILES=NUMBER_OF_FILES+1
-            echo "[X] => $NUMBER_OF_FILES : $file"
-            if [ $CHECK1 = 'Uptime' ]; then
+            if [ $CHECK1 = $SEARCHING_WORD ]; then
+                echo "=> [-]   $NUMBER_OF_FILES : $file" | lolcat
                 rm -rf $file
             fi
         fi
